712532fbd83aa17ba48ccf7dbf83087351930a21
pwli <Dev-L@outlook.com>
2020-03-26 14:42:39 +0800

    Update 53. Maximum Subarray.go

--- array/53. Maximum Subarray.go
+++ array/53. Maximum Subarray.go
@@ -14,3 +14,49 @@ func maxSubArray(nums []int) int {
 }
 
 
+
+/* 
+// solution 2：
+// divide and conquer
+
+func maxSubArray(nums []int) int {
+    return divideAndConquer(nums, 0, len(nums)-1)
+}
+
+func divideAndConquer(nums []int, left, right int) int{
+    if left == right{
+        return nums[left]
+    }
+    mid := (left+right)/2
+    lMax := divideAndConquer(nums, left, mid)
+    rMax := divideAndConquer(nums, mid+1, right)
+    crossLeft := math.MinInt32
+    crossRight := math.MinInt32
+    crossSum := 0
+    for i:=mid;i>=left;i--{
+        crossSum += nums[i]
+        if crossSum > crossLeft{
+            crossLeft = crossSum
+        }
+    }
+    crossSum = 0
+    for i:=mid+1;i<=right;i++{
+         crossSum += nums[i]
+        if crossSum > crossRight{
+            crossRight = crossSum
+        }
+    }
+    return max(lMax, rMax, crossLeft+crossRight)
+}
+
+func max(args ...int) int{
+    ret := math.MinInt32
+    for _, v := range args{
+        if v > ret{
+            ret = v
+        }
+    }
+    return ret
+}
+
+*/
