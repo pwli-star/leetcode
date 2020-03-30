9a2ac62c918c8830d25c2117566080916beb1bae
pwli <Dev-L@outlook.com>
2020-03-30 19:47:47 +0800

    Create 208. Implement Trie (Prefix Tree).py

---
+++ Trie/208. Implement Trie (Prefix Tree).py
@@ -0,0 +1,43 @@
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+        
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        cur = self.root
+        for c in word:
+            if c not in cur:
+                cur[c] = {}
+            cur = cur[c]
+        cur['#'] = '#'
+        
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        cur = self.root
+        for c in word:
+            if c not in cur:
+                return False
+            cur = cur[c]
+        return '#' in cur
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        cur = self.root
+        for c in prefix:
+            if c not in cur:
+                return False
+            cur = cur[c]
+        return True
