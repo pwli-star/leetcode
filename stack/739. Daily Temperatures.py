411edaa5467f6677b795a7ea75670d53764dd4f6
pwli <Dev-L@outlook.com>
2020-04-08 14:47:50 +0800

    Create 739. Daily Temperatures.py

---
+++ stack/739. Daily Temperatures.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        res = [0] * len(T)
+        stack = []
+        for i, t in enumerate(T):
+            while stack and T[stack[-1]] < t:
+                cur = stack.pop()
+                res[cur] = i - cur
+            stack.append(i)
+        return res
